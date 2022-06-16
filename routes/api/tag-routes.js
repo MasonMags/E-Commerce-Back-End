edd41a71d6ffa2c43c0368259fd2e279686c056e
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-06-16 14:43:51 -0500

    created post route for tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,19 +2,49 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+// find all tags
+// be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findall({
+      include: [{ model: Product },
+      { model: ProductTag }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err)
+  } 
 });
 
+// find a single tag by its `id`
+  // be sure to include its associated Product data
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product },
+        { model: ProductTag }],
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag with this id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+// create a new tag
 router.post('/', (req, res) => {
-  // create a new tag
+  try {
+    const tagData = await Tag.create({
+      //id: req.body.id, <-- possibly don't need since id is set to auto-increment
+      tag_name: req.body.tag_name,
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
