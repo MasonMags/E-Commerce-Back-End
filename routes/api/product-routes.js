0705852466df9ceacf83ef128b18ce202ba27cbd
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-06-16 13:24:42 -0500

    updated comments for category routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -38,8 +38,7 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
-router.post('/', (req, res) => {
-  /* req.body should look like this...
+ /* req.body should look like this...
     {
       product_name: "Basketball",
       price: 200.00,
@@ -47,6 +46,7 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+router.post('/', (req, res) => {
   Product.create({
     product_name: req.body.product_name,
     price: req.body.price,
@@ -54,6 +54,7 @@ router.post('/', (req, res) => {
     category_id: req.body.category_id,
     tagIds: req.body.tagIds
   })
+
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
